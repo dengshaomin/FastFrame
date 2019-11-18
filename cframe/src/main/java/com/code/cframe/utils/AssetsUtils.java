62b5dfcf69dc9062f88eb57a630ce9527d44b192
coder <nocoding@163.com>
2019-11-18 21:08:56 +0800

    add assets utils

--- cframe/src/main/java/com/code/cframe/utils/AssetsUtils.java
+++ cframe/src/main/java/com/code/cframe/utils/AssetsUtils.java
@@ -1,30 +1,55 @@
 package com.code.cframe.utils;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by dengshaomin on 2017/12/6.
  */
 
 public class AssetsUtils {
-
-    public static String getFromAssets(Context context, String fileName) {
+    /*
+     * 从Assets中读取文件
+     */
+    public static String readFile(Context context, String fileName) {
+        String result = "";
         try {
             InputStreamReader inputReader = new InputStreamReader(context.getResources().getAssets().open(fileName));
             BufferedReader bufReader = new BufferedReader(inputReader);
             String line = "";
-            String Result = "";
             while ((line = bufReader.readLine()) != null) {
-                Result += line;
+                result += line;
             }
-            return Result;
+            inputReader.close();
+            bufReader.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+        return result;
+    }
+    /*
+     * 从Assets中读取图片
+     */
+    public static  Bitmap readBitmap(Context context,String fileName) {
+        Bitmap image = null;
+        try {
+            AssetManager am = context.getResources().getAssets();
+            InputStream is = am.open(fileName);
+            image = BitmapFactory.decodeStream(is);
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+
+
     }
 
 }
